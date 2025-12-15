dfa46eb5ce4a9a892e39b4d531fe264e06f0a5dd
Muhammad Faraz Ahsan <mfarazahsan710@gmail.com>
2025-12-15 15:26:12 +0500

    resolve a bug

--- backend/src/server.js
+++ backend/src/server.js
@@ -52,7 +52,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ 
     success: false, 
     message: 'Something went wrong!',
-    error: process.env.NODE_ENV === 'development' ? err.message : undefined
   });
 });
 
